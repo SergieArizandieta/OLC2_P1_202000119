74d5ae0dacaecca7ece1aacbb8c7aa76eb020421
Serch-ING <3000523830101@ingenieria.usac.edu.gt>
2022-08-19 18:12:47 -0500

    se intento mejorar gramatica pero no se pudo
    Se dejo la nueva gramatica ya que es mas simple de leer

--- AST/Instruccion/Match/BloqueMatch.py
+++ AST/Instruccion/Match/BloqueMatch.py
@@ -14,14 +14,19 @@ class BloqueMatch(Intruccion,Expresion):
         retorno = None
         print("== Ejcutando intruccion ==")
         for intruccion in self.instrucciones:
-            retorno = intruccion.EjecutarInstruccion(controlador, ts_local)
+            try:
+                retorno = intruccion.EjecutarInstruccion(controlador, ts_local)
+            except: pass
         return retorno
 
     def ObtenerValor(self, controlador, ts):
         retorno = None
         print("== Ejcutando Obtener valor ==")
         for intruccion in self.instrucciones:
-            retorno = intruccion.ObtenerValor(controlador, ts)
+            try:
+                retorno = intruccion.ObtenerValor(controlador, ts)
+            except:
+                intruccion.EjecutarInstruccion(controlador, ts)
         return retorno
 
     def ObtenerTipo(self, controlador, ts):
