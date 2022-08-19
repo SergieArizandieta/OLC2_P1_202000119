162dbb9502d9eeccc223caccd92aac7cd4c0522b
Serch-ING <3000523830101@ingenieria.usac.edu.gt>
2022-08-19 03:50:06 -0500

    agregando clase if

---
+++ AST/Instruccion/SentenciasControl/ifs.py
@@ -0,0 +1,12 @@
+from AST.Abstracto.Instruccion import Intruccion
+from AST.Abstracto.Expresion import Expresion
+
+class Ifs(Intruccion,Expresion):
+    def EjecutarInstruccion(self, controlador, ts):
+        pass
+
+    def ObtenerValor(self, controlador, ts):
+        pass
+
+    def ObtenerTipo(self, controlador, ts):
+        pass
